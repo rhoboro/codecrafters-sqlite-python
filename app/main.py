969c844f05fe4cfc3cc490b3058eca74b21576f2
rhoboro <rhoboro@gmail.com>
2023-01-07 09:55:56 +0900

    Print page size

--- app/main.py
+++ app/main.py
@@ -1,8 +1,40 @@
 import sys
 
 from dataclasses import dataclass
+from struct import unpack
+
 
 # import sqlparse - available if you need it!
+@dataclass(frozen=True)
+class Header:
+    magic_number: str
+    page_size: int
+    write_format: int
+    read_format: int
+    unused_reserved_space: int
+    maximum_embedded_payload_fraction: int
+    minimum_embedded_payload_fraction: int
+    leaf_payload_fraction: int
+    file_change_counter: int
+    in_header_database_size: int
+    first_freelist_trunk_page: int
+    freelist_pages: int
+    schema_cookie: int
+    schema_format_number: int
+    default_page_cache_size: int
+    largest_root_btree_page: int
+    database_text_encoding: int
+    user_version: int
+    is_incremental_vacuum_mode: int
+    application_id: int
+    version_valid_for_number: int
+    sqlite_version_number: int
+
+
+def parse_header(raw_header: bytes) -> Header:
+    parsed = unpack(">16shbbbbbbIIIIIIIIIIII20xII", raw_header)
+    return Header(*parsed)
+
 
 database_file_path = sys.argv[1]
 command = sys.argv[2]
@@ -11,6 +43,8 @@ if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
         # You can use print statements as follows for debugging, they'll be visible when running tests.
         print("Logs from your program will appear here!")
+        header = parse_header(database_file.read()[:100])
+        print(f"database page size: {header.page_size}")
 
         # Uncomment this to pass the first stage
         # database_file.seek(16)  # Skip the first 16 bytes of the header
